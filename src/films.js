a97e2548cbfe06e558a3109effee3bf4f9d42723
Mika7596 <mika7596@gmail.com>
2025-02-26 10:20:43 +0100

    Linked the JS files to index.html to see the results; Created first function (exercise 1)

--- src/films.js
+++ src/films.js
@@ -1,10 +1,13 @@
 // Exercise 1: Get the array of all directors.
+
+// const movies = require("./data")
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result =  [];
+  array.map(movie => result.push(movie.director));
+  console.log ("EXERCICE 1 ->", result);
   return result;
 }
-
+getAllDirectors(movies)
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
  
@@ -31,14 +34,45 @@ function moviesAverageByCategory() {
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-function hoursToMinutes() {
-
-}
+// function hoursToMinutes(array) {
+//   let hours;
+//   let minutes;
+//   let durations = array.map( movie => {
+//     if (!movie.duration.includes("h")){
+//       hours = 0;
+//     } else{
+//       hours = Number(movie.duration.trim().slice(0, movie.duration.trim().indexOf("h")))
+//     }
+//     if (!movie.duration.includes("m")){
+//       minutes = 0;
+//     } else{
+//       minutes = Number(movie.duration.slice(movie.duration.indexOf("m")-2, movie.duration.indexOf("m")));
+//     }
+//     let durationInMinutes = hours * 60 + minutes;
+//     return {
+//       ...movie,
+//       duration: durationInMinutes
+//     }
+//   });
+//   console.log("Exercise 7 ->");
+//   console.log(durations);
+// }
+// hoursToMinutes(movies)
 
 // Exercise 8: Get the best film of a year
-function bestFilmOfYear() {
-  
-}
+// function bestFilmOfYear(array, year) {
+//   let moviesOfYear = array.filter( movie => movie.year == year);
+//   let highestScore = 0;
+//   let bestFilm;
+//   moviesOfYear.forEach( movie => {
+//     if (movie.score > highestScore){
+//       bestFilm = movie;
+//     }
+//   })
+//   console.log(bestFilm)
+// }
+
+// bestFilmOfYear(movies, 1984)
 
 
 

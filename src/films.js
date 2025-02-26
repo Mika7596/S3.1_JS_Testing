f4f6f32ed542edf8a99a5af7afdfce107479bc8d
Mika7596 <mika7596@gmail.com>
2025-02-26 11:09:47 +0100

    Fixed a problem to run the test; Second exercise added.

--- src/films.js
+++ src/films.js
@@ -1,17 +1,21 @@
-// Exercise 1: Get the array of all directors.
+const movies = require('./data');
 
-// const movies = require("./data")
+// Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
   let result =  [];
   array.map(movie => result.push(movie.director));
   console.log ("EXERCICE 1 ->", result);
   return result;
 }
-getAllDirectors(movies)
+getAllDirectors(movies);
+
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  const moviesOf = array.filter(movie => movie.director == director);
+  console.log("EXERCISE 2 -> ", moviesOf);
+  return moviesOf
 }
+getMoviesFromDirector(movies, "Frank Darabont")
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
@@ -34,7 +38,7 @@ function moviesAverageByCategory() {
 }
 
 // Exercise 7: Modify the duration of movies to minutes
-// function hoursToMinutes(array) {
+function hoursToMinutes(array) {
 //   let hours;
 //   let minutes;
 //   let durations = array.map( movie => {
@@ -56,11 +60,11 @@ function moviesAverageByCategory() {
 //   });
 //   console.log("Exercise 7 ->");
 //   console.log(durations);
-// }
+ }
 // hoursToMinutes(movies)
 
 // Exercise 8: Get the best film of a year
-// function bestFilmOfYear(array, year) {
+function bestFilmOfYear(array, year) {
 //   let moviesOfYear = array.filter( movie => movie.year == year);
 //   let highestScore = 0;
 //   let bestFilm;
@@ -70,7 +74,7 @@ function moviesAverageByCategory() {
 //     }
 //   })
 //   console.log(bestFilm)
-// }
+}
 
 // bestFilmOfYear(movies, 1984)
 
@@ -87,6 +91,7 @@ if (typeof module !== 'undefined') {
     orderByYear,
     moviesAverageByCategory,
     hoursToMinutes,
-    bestFilmOfYear,
+    bestFilmOfYear
   };
 }
+console.log('first commit');
